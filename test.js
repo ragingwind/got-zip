fc66526cd8afa5ed2d31b125b2776342cfce844e
ragingwind <ragingwind@gmail.com>
2016-03-06 07:32:01 +0900

    Using existsSync

--- test.js
+++ test.js
@@ -9,19 +9,21 @@ import path from 'path';
 import fs from 'fs';
 
 const zip = 'https://github.com/ragingwind/got-zip/archive/v0.2.2.zip';
-var opts = {
+const opts = {
 	dest: path.join(__dirname, './.tmp')
 };
-var filename = path.join(opts.dest, path.basename(zip));
+const existsSync = function (filename) {
+	return fs.existsSync(path.join(opts.dest, filename));
+}
 
-test.beforeEach(function() {
+test.beforeEach(function () {
 	rimraf.sync(opts.dest);
 	mkdirp(opts.dest);
 });
 
 test.serial('should download, extact and cleanup a zip', t => {
 	return gotZip(zip, opts).then(() => {
-		t.ok(!fs.existsSync(filename));
+		t.ok(!existsSync(path.basename(zip)));
 	});
 });
 
@@ -29,7 +31,7 @@ test.serial('should exist a zip', t => {
 	opts.cleanup = false;
 
 	return gotZip(zip, opts).then(() => {
-		t.ok(fs.existsSync(filename));
+		t.ok(existsSync(path.basename(zip)));
 	});
 });
 
@@ -40,7 +42,7 @@ test.serial('should exclude all of the files', t => {
 	];
 
 	return gotZip(zip, opts).then(() => {
-		t.ok(!fs.existsSync(path.join(opts.dest, 'package.json')));
-		t.ok(!fs.existsSync(path.join(opts.dest, 'readme.md')));
+		t.ok(!existsSync('package.json'));
+		t.ok(!existsSync('readme.md'));
 	});
 });
